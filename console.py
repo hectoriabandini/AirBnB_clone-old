6df276b7b4b9e139e0ac957da879abe7beac529d
Nancyenos <nicymimz@gmail.com>
2023-08-10 05:00:11 -0700

    create

--- console.py
+++ console.py
@@ -2,10 +2,13 @@
 
 """ The console! command line interpreter for HBnB """
 import cmd
-
+from models import base_model
+import json
+import os.path
 class HBNBCommand(cmd.Cmd):
     """ console class """
     prompt = "(hbnb)"
+    class_mapping = {"BaseModel": base_model.BaseModel}
 
     def do_quit(self, arg):
         """ Quit command to exit the program """
@@ -17,8 +20,17 @@ class HBNBCommand(cmd.Cmd):
         return True
     def emptyline(self):
         pass
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if not arg:
+            print("* class name missing *")
+            return
 
-
-
+        try:
+            new_instance = self.class_mapping[arg]()
+            new_instance.save()
+            print(new_instance.id)
+        except:
+            print("* class doesn't exist *")
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
