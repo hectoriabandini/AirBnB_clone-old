7cb2b2ee848910806fae82f39917850a56778d3b
Nancyenos <nicymimz@gmail.com>
2023-08-15 04:00:21 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,45 @@
+#!/usr/bin/python3
+'''module base_model'''
+
+
 import uuid
 from datetime import datetime
 import models
 
 
-class BaseModel:
+class BaseModel():
+    """ parent class BaseModel"""
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        """ initialize BaseModel"""
         if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key,
-                            datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
+                if key != '__class__':
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
-        clname = self.__class__.__name__
-        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+        """ string represantation od basemodel """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
+        """ saves and updates date and time """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        new_dict = self.__dict__.copy()
+        """dictionary representation of BaseModel """
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        new_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
         new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        return (new_dict)
